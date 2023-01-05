d87162c016c26844c97d48d557876d1a6915d23c
danielzhang183 <danielzhang182@gmail.com>
2023-01-05 12:09:53 +0800

    feat(compiler): parse element

---
+++ packages/compiler/src/parse.ts
@@ -0,0 +1,135 @@
+enum TEXTMODES {
+  DATA = 'DATA',
+  RCDATA = 'RCDATA',
+  RAWTEXT = 'RAWTEXT',
+  CDATA = 'CDATA',
+}
+
+interface ParseContext {
+  source: string
+  mode: TEXTMODES
+  advanceBy: (num: number) => void
+  advanceSpaces: () => void
+}
+
+interface Element {
+  type: 'Element'
+  tag: string
+  props?: any[]
+  children?: Element[]
+  isSelfClosing: boolean
+}
+
+export function parse(str: string) {
+  const context: ParseContext = {
+    source: str,
+    mode: TEXTMODES.DATA,
+    advanceBy(num) {
+      context.source = context.source.slice(num)
+    },
+    advanceSpaces() {
+      const match = /^[\t\r\n\f]/.exec(context.source)
+      if (match)
+        context.advanceBy(match[0].length)
+    },
+  }
+
+  const nodes = parseChildren(context, [])
+  return {
+    type: 'Root',
+    children: nodes,
+  }
+}
+
+function parseChildren(context: ParseContext, ancestors: Element[]) {
+  const nodes = []
+  const { source, mode } = context
+
+  while (!isEnd(context, ancestors)) {
+    let node
+
+    if (mode === TEXTMODES.DATA || mode === TEXTMODES.RCDATA) {
+      if (mode === TEXTMODES.DATA && source[0] === '<') {
+        if (source[1] === '!') {
+          if (source.startsWith('<!--'))
+            node = parseComment(context)
+          else if (source.startsWith('<![CDATA['))
+            node = parseCDATA(context, ancestors)
+        }
+        else if (source[1] === '/') {
+          console.error('Invalid end tag')
+        }
+        else if (/[a-z]/i.test(source[1])) {
+          node = parseElement(context, ancestors)
+        }
+      }
+      else if (source.startsWith('{{')) {
+        node = parseInterpolation(context)
+      }
+    }
+
+    if (!node)
+      node = parseText(context)
+
+    nodes.push(node)
+  }
+
+  return nodes
+}
+
+function parseTag(context: ParseContext, type = 'start') {
+  const { advanceBy, advanceSpaces, source } = context
+
+  const match = type === 'start'
+    ? /^<([a-z][^\t\r\n\f />]*)/i.exec(source)
+    : /^<\/([a-z][^\t\r\n\f />]*)/i.exec(source)
+
+  const tag = match?.[1]
+  advanceBy(match![0].length)
+  advanceSpaces()
+
+  const isSelfClosing = source.startsWith('/>')
+  advanceBy(isSelfClosing ? 2 : 1)
+
+  return {
+    type: 'Element',
+    tag,
+    props: [],
+    children: [],
+    isSelfClosing,
+  }
+}
+
+function isEnd(context: ParseContext, ancestors: Element[]) {
+  if (!context.source)
+    return true
+
+  for (let i = ancestors.length - 1; i >= 0; --i) {
+    if (context.source.startsWith(`</${ancestors[i].tag}`))
+      return true
+  }
+}
+
+function parseElement(context: ParseContext, ancestors: Element[]) {
+  const element = parseTag(context)
+  if (element.isSelfClosing)
+    return element
+
+  if (element.tag === 'textarea' || element.tag === 'title')
+    context.mode = TEXTMODES.RCDATA
+  else if (/style|xmp|iframe|noembed|noframes|noscript/.test(element.tag!))
+    context.mode = TEXTMODES.RAWTEXT
+  else
+    context.mode = TEXTMODES.DATA
+
+  ancestors.push(element)
+  element.children = parseChildren(context, ancestors)
+  ancestors.pop()
+
+  if (context.source.startsWith(`</${element.tag}`))
+    parseTag(context, 'end')
+  else
+    console.error(`${element.tag} lack of end tag`)
+
+  return element
+}
